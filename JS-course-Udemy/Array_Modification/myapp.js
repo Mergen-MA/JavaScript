dd842856c664eb36148c1e0e31ea428930da1483
Mergen-MA <malidinc13@gmail.com>
2023-08-19 15:01:37 +0200

    Callable Objects

--- JS-course-Udemy/Array_Modification/myapp.js
+++ JS-course-Udemy/Array_Modification/myapp.js
@@ -7,3 +7,10 @@ var array = [
 
 ];
 
+// Javascript Methods
+
+// shift() - removes the first element of an array
+// unshift() - add elements into the arrary
+// Pop() - removes the last element of an array
+// Splice() - removes elements from where we want ArrayBuffer.
+// splice(2,2, 300, 129) : from the 2nd element remove 2 and add the subsequent elements.
---
+++ JS-course-Udemy/Callable_Objects/myapp.js
@@ -0,0 +1,28 @@
+function name() {
+    var fullname = "Mali Dinc";
+
+    function concat( name) {
+        return "Mr." + name;
+    }
+
+    return concat(fullname);
+}
+
+
+
+function name1 ( fullname) {
+    return fullname.firstname + "" + fullname.lastname;
+
+}
+
+name1( { firstname:"Mali", lastname:"Dinc"});
+
+// Callable Object -- functions are callable object.
+
+function name2 ( fullname ) {
+    return fullname();
+}
+
+
+
+console.log(name2( function(){ return "emdeb";}));
